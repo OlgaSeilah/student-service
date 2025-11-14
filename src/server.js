c4defcc7899724d615f6ba343acf949813d39701
seilah <princenoru@gmail.com>
2025-11-14 18:38:55 +0100

    homework_06_mongo_storage: created server func

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import studentRoutes from './routes/studentRoutes.js'
+import {MongoClient} from "mongodb";
+import {initDb} from "./repository/studentRepository.js";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
+const client = new MongoClient(process.env.MONGO_URI);
 
 app.use(express.json());
 app.use(studentRoutes);
@@ -12,7 +15,18 @@ app.use((req, res) => {
     res.status(404).json({message: 'Not found'})
 })
 
+async function startServer() {
+    try {
+        await client.connect();
+        const database = client.db(process.env.DB_NAME);
+        initDb(database);
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-});
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`)
+        });
+    } catch (error) {
+        console.log(`Failed connecting to MongoDB: ${error.message}`);
+    }
+}
+
+startServer();
